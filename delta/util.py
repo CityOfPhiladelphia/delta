80c2277255e7f730a424df7c85202a962a83f8ec
Robert Martin <rmartin@rmart.in>
2016-09-07 11:09:46 -0400

    Fix module loading helper

--- delta/util.py
+++ delta/util.py
@@ -10,7 +10,7 @@ def import_module_from_file(name, path):
         import importlib.util
         spec = importlib.util.spec_from_file_location(name, path)
         mod = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(foo)
+        spec.loader.exec_module(mod)
     elif (3, 3) <= py_version <= (3, 4):
         from importlib.machinery import SourceFileLoader
         mod = SourceFileLoader(name, path).load_module()
